8e8c1c76ef9730368df6fdfc99599cd29d6f27c2
Rolandas Hauser <70347666+hausra5@users.noreply.github.com>
2022-08-15 14:52:48 -0400

    Added pricing file location variables.

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from builder.builder import builder
 
+front_prices = 'qcsp_prices/Front_Print.csv'
+addl_prices = 'qcsp_prices/addl_print.csv'
+
 if __name__ == '__main__':
-    builder(front_price='qcsp_prices/Front_Print.csv', addl_price='qcsp_prices/addl_print.csv')
+    builder(front_price=front_prices, addl_price=addl_prices)
